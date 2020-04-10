bd94c030104a8cab7c9fc77d8a6ed11d62f08d0e
Douglas Locklin <douglas.locklin@gmail.com>
2020-04-10 13:57:46 +0300

    Create treeArt.java

---
+++ treeArt.java
@@ -0,0 +1,17 @@
+// Define your tree class in this file!
+
+public class Tree {
+  public static void main(String[] args) {
+    // I will introduce myself and plant a tree. 
+    System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!");
+    System.out.println("Ready to get my hands dirty!");
+    System.out.println();
+    System.out.println();
+    System.out.println("         *  ");
+    System.out.println("        *** ");
+    System.out.println("       ***** ");
+    System.out.println("      ******* ");
+    System.out.println("        ***  ");
+    System.out.println("        ***  ");   
+  }
+}
